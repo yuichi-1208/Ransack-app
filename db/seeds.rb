9d2e15bbc5e45be0a357bf0192e3ab084f09aebf
yuichi oyama <yuichi19991208@gmail.com>
2020-11-13 11:50:12 +0900

    seedデータを挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Book.create(title: 'ワンピース', auther: '尾田栄一郎', price: '400')
+Book.create(title: 'ナルト', auther: '岸本斉史', price: '300')
+Book.create(title: '鬼滅の刃', auther: '吾峠呼世晴', price: '480')
+Book.create(title: '進撃の巨人', auther: '諫山創', price: '450')
+Book.create(title: 'キングダム', auther: '原泰久', price: '800')
+Book.create(title: '７つの大罪', auther: '鈴木央', price: '1000')
+Book.create(title: 'ジャイアントキリング', auther: 'ツジトモ', price: '2500')
+Book.create(title: 'ガンツ', auther: '奥浩哉', price: '100')
+Book.create(title: 'テラフォーマーズ', auther: '貴家悠', price: '1800')
+Book.create(title: 'いぬやしき', auther: '奥浩哉', price: '700')
